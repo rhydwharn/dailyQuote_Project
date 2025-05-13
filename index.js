70deddef5bd156a5f8327b383620749df9578ca2
rhydwharn <ridwan.abdulazeez1@gmail.com>
2025-05-13 20:54:58 +0100

    updated routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express"
+import quotesRouter from "./routes/quotes.js"
 
 const app = express()
 const PORT  = 1850
 app.use(express.json())
+
+app.use('/api/quotes', quotesRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
